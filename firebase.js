121f15f77c7aa4b3688d5da2208f4b2729df79f8
Ksm <the13mk@gmail.com>
2025-07-05 18:31:41 +0530

    Added Firebase Connection

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAspMAMC-z-82F9V7EBN-wiy87mSlyGeLI",
+    authDomain: "complain-box-282e1.firebaseapp.com",
+    projectId: "complain-box-282e1",
+    storageBucket: "complain-box-282e1.firebasestorage.app",
+    messagingSenderId: "891659088096",
+    appId: "1:891659088096:web:28d387dba11414c5e5e085"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
